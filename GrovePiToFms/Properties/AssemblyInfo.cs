a417140461a3a4c0d3d0ac5f8369f59aa87887b6
Wim Decorte <wim@connectingdata.com>
2017-09-22 12:04:34 -0400

    updated nuget fmdotnet for logout bug, changed to logging out after 12 minutes

--- GrovePiToFms/Properties/AssemblyInfo.cs
+++ GrovePiToFms/Properties/AssemblyInfo.cs
@@ -6,11 +6,11 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("GrovePiToFms")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("GrovePi Hat collecting data to send to FMS16+")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("Connecting Data Inc.")]
 [assembly: AssemblyProduct("GrovePiToFms")]
-[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyCopyright("Copyright © Connecting Data Inc.,  2017")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
--- GrovePiToFms/StartupTask.cs
+++ GrovePiToFms/StartupTask.cs
@@ -1,9 +1,15 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Net.Http;
+using System.Net;
 using Windows.ApplicationModel.Background;
+using Windows.System.Threading;
+using FMdotNet__DataAPI;
+using Windows.ApplicationModel.Resources;
+using Windows.UI;
+using System.Threading;
+using GrovePi;
+using GrovePi.Sensors;
+using Raspberry;
+
 
 // The Background Application template is documented at http://go.microsoft.com/fwlink/?LinkID=533884&clcid=0x409
 
@@ -11,6 +17,20 @@ namespace GrovePiToFms
 {
     public sealed class StartupTask : IBackgroundTask
     {
+
+        BackgroundTaskDeferral _deferral;
+
+        FMS fmserver;
+        DateTime tokenRecieved;
+        string token;
+
+        ILightSensor lightSensor;
+        ISoundSensor soundSensor;
+        ILedBar leds;
+
+        DateTime start;
+        DateTime end;
+
         public void Run(IBackgroundTaskInstance taskInstance)
         {
             // 
@@ -20,6 +40,124 @@ namespace GrovePiToFms
             // from closing prematurely by using BackgroundTaskDeferral as
             // described in http://aka.ms/backgroundtaskdeferral
             //
+
+            _deferral = taskInstance.GetDeferral();
+
+            // set the security for fms, and hook into fms
+            ServicePointManager.SecurityProtocol |= SecurityProtocolType.Tls11 | SecurityProtocolType.Tls12;
+
+            // hook into FMS from settings in the config file
+            var resources = new ResourceLoader("config");
+            var fm_server_address = resources.GetString("fm_server_address");
+            var fm_file = resources.GetString("fm_file");
+            var fm_layout = resources.GetString("fm_layout");
+            var fm_account = resources.GetString("fm_account");
+            var fm_pw = resources.GetString("fm_pw");
+            var start_hour = resources.GetString("start_time");
+            var end_hour = resources.GetString("end_time");
+
+            // calculate the current day's start and end time
+
+            fmserver = new FMS(fm_server_address, fm_account, fm_pw);
+            fmserver.SetFile(fm_file);
+            fmserver.SetLayout(fm_layout);
+
+            // get the sensors
+            lightSensor = DeviceFactory.Build.LightSensor(Pin.AnalogPin0);
+            soundSensor = DeviceFactory.Build.SoundSensor(Pin.AnalogPin2);
+            leds = DeviceFactory.Build.BuildLedBar(Pin.DigitalPin8);
+
+            // start the timer
+            ThreadPoolTimer timer = ThreadPoolTimer.CreatePeriodicTimer(Timer_Tick, TimeSpan.FromSeconds(240));
+        }
+
+        private async void Timer_Tick(ThreadPoolTimer timer)
+        {
+
+            // record the start time
+            DateTime start = DateTime.Now;
+
+            // update the display
+            leds.Initialize(GrovePi.Sensors.Orientation.GreenToRed);
+            leds.SetLevel((byte)0);
+
+
+            int lightValue = 0;
+            string lightError = string.Empty;
+
+            // read the light
+            try
+            {
+                lightValue = lightSensor.SensorValue();
+                // int between 0 (dark) and 1023 (bright) 
+            }
+            catch (Exception ex)
+            {
+                lightError = ex.Message;
+            }
+
+            int soundValue = 0;
+            string soundError = String.Empty;
+            try
+            {
+                soundValue = soundSensor.SensorValue();
+            }
+            catch(Exception ex)
+            {
+                soundError = ex.Message;
+            }
+
+            // check if token is still valid
+            // get a new token every 12 minutes - no real need to since we'll usually keep the connection alive
+            if ( token == null || token == string.Empty)
+            {
+                token = await fmserver.Authenticate();
+                tokenRecieved = DateTime.Now;
+            }
+            else if (DateTime.Now > tokenRecieved.AddMinutes(12))
+            {
+                await fmserver.Logout();
+                token = string.Empty;
+                token = await fmserver.Authenticate();
+                tokenRecieved = DateTime.Now;
+            }
+            if (token != string.Empty)
+            {
+
+                // get some data from the RPI itself
+                var processorName = Raspberry.Board.Current.ProcessorName;
+                var rpiModel = Raspberry.Board.Current.Model.ToString();
+
+                // write it to FMS
+                var request = fmserver.NewRecordRequest();
+
+                if (processorName != null)
+                    request.AddField("rpiProcessor", processorName);
+
+                if (rpiModel != null)
+                    request.AddField("rpiModel", rpiModel);
+
+                request.AddField("when_start", start.ToString());
+                request.AddField("sound", soundValue.ToString());
+                request.AddField("sound_error", soundError);
+                request.AddField("light", lightValue.ToString());
+                request.AddField("light_error", lightError);
+                request.AddField("when", DateTime.Now.ToString());
+
+
+                var response = await request.Execute();
+                if (response.errorCode != 0)
+                {
+                    leds.SetLevel((byte)10);
+                    Thread.Sleep(TimeSpan.FromMilliseconds(250));
+
+                }
+                await fmserver.Logout();
+            }
+            // clear the display again
+            leds.SetLevel((byte)1);
+            Thread.Sleep(TimeSpan.FromMilliseconds(250));
+
         }
     }
 }
